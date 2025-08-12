05a6c1b66364b44f2c49e21116ee29a16e0491b1
Jenser Majar <jensernajar228@gmail.com>
2025-08-12 13:33:56 -0400

    reparacion d imagen

--- llenar.js
+++ llenar.js
@@ -2,27 +2,23 @@
 const servicios = [
     {
         nombre: "Taper",
-        fotos: ["img/01.jpg", "img/02.jpg",
-                "img/03.webp", "img/04.jpg",
-                "img/05.jpg", "img/06.jpg",
-                "img/09.jpg", "img/10.jpg",
-                "img/11.jpg",],
+        fotos: ["img/01.jpg", "img/03.webp",
+            "img/09.jpg", "img/10.jpg",
+            "img/11.jpg",],
         descripcion: "Taper fade",
-        precio: "C$80"
+        precio: "C$100"
     },
     {
         nombre: "low fade",
-        fotos: ["img/07.jpg", "img/08.jpg",
-                "img/03.webp", "img/04.jpg",
-                "img/05.jpg", "img/06.jpg",],
+        fotos: ["img/07.jpg", "img/08.jpg",],
         descripcion: "low fade",
-        precio: "C$80"
+        precio: "C$100"
     },
     {
         nombre: "Mohicano",
         fotos: ["img/02.jpg", "img/05.jpg",
-                "img/04.jpg"],
+            "img/04.jpg", "img/06.jpg",],
         descripcion: "Mohicano",
-        precio: "C$80"
+        precio: "C$100"
     },
 ];
